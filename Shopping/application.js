6092e9a358f3fe9f273f42fd64ba1d0a5c206f95
Ithomas220 <Ithomas@kippnyccp.org>
2015-03-23 20:51:17 -0400

    Update application.js

--- Shopping/application.js
+++ Shopping/application.js
@@ -1,1 +1,11 @@
+$(document).ready(function(){
+$('#enter').on('click',function(){
+   var item = $('input').val();
+   $('ul').append('<li>'+item+"</li>");
+   });
+$('#empty').on('click',function(){
+   var item = $('input').val();
+   $('ul').empty();
+ });
+});
 
