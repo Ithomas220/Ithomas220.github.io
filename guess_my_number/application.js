b52793af58622d85d694f0dc1d4efbbf63f3a3d9
Ithomas220 <Ithomas@kippnyccp.org>
2015-04-14 08:43:06 -0400

    Update application.js

--- guess_my_number/application.js
+++ guess_my_number/application.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   $('button').on('click', function(){
     //save their guess as a variable
     var userGuess 
-    console.log($('input').val)
+    console.log($('input').val())
     //and compare this guess to the random number that the computer picked
     //if the user guessed the correct number..
     if (userGuess === randomindex) {
