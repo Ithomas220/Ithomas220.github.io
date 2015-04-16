57ac53134a69670ce2b86c1debdb8230cf498aa2
Ithomas220 <Ithomas@kippnyccp.org>
2015-04-16 08:41:30 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -7,20 +7,23 @@ $(document).ready(function(){
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   };
   $('.card').click(function(){
-    if(click_count == 0){
+    if(click_count == 0){ //When the person clicks on the first card
   	$(this).find('p').css('opacity', 1);
-    $(this).find('p').addClass('clicked');  
-      click_count = 1;
-      click_1 = $(this).find('p').text();
+    	$(this).find('p').addClass('clicked');  
+      	click_count = 1;
+      	click_1 = $(this).find('p').text();
     }
-    else{	
+    else{	// when the person clicks on the second card
       $(this).find('p').css('opacity', 1);
       click_count = 0;
       click_2 = $(this).find('p').text();
       $(this).find('p').addClass('clicked');
-      if(click_1 != click_2){
-         setTimeout(function(){$('.clicked').css('opacity', '0').removeClass('clicked')}, 1000);
+      if(click_1 == click_2){
+        $('.clicked').css('color', 'blue').removeClass('clicked');
       }
+      else{//if not the same then disappear
+         setTimeout(function(){$('.clicked').css('opacity', '0').removeClass('clicked')}, 1000);
+      };
     };
-	});
+  });
 });
