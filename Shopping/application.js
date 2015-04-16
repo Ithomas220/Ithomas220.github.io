b8b30288c5a990d183414cada51a852e0e161f83
Ithomas220 <Ithomas@kippnyccp.org>
2015-04-16 08:30:22 -0400

    Update application.js

--- Shopping/application.js
+++ Shopping/application.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-$('#enter').on('click',function(){
-var item = $('select option:selected').val();
-$('ul').append('<li>'+item+"</li>");
- $('#empty').on('click',function(){
-var item = $('input').val();
- });
+  $('#submit').on('click', function(){
+    var entry = $('input').val();
+    $('ul').append("<li>"+entry+"</li>"); //the item will be appended into the ul as a li element.
+  })
+  $('#clear').on('click', function(){
+    var entry = $('input').val();
+    $("ul").empty(); //the items will be removed from the ul.
+  })
 });
-
 
